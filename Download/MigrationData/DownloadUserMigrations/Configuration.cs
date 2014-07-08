f37556e478db0fd9e6fb49571eb418db99622caf
Ethan Johnson <ethan@stonefin.com>
2014-07-08 10:33:10 -0500

    Trying to add code first migration files that structured the online database

---
+++ Download/MigrationData/DownloadUserMigrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Download.MigrationData.DownloadUserMigrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Download.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            MigrationsDirectory = @"MigrationData\DownloadUserMigrations";
+        }
+
+        protected override void Seed(Download.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
