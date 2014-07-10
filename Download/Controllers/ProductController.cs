c1ad1d5337c38fe6ba28af082ac51f79117dee1e
Ethan Johnson <ethan@stonefin.com>
2014-07-10 14:25:42 -0500

    Changed the encoding of the readme yet again

--- Download/Controllers/ProductController.cs
+++ Download/Controllers/ProductController.cs
@@ -189,7 +189,7 @@ namespace Download.Controllers
                                         //Get the readme
                                         var file = GetBlob("file", arch.ReadMe);
                                         //Convert the byte[] to text so the markdown converter will work
-                                        var Read = System.Text.Encoding.UTF8.GetString(file.ToArray());
+                                        var Read = System.Text.Encoding.GetEncoding(1252).GetString(file);
                                         ViewData["Content"] = md.Transform(Read);
                                     }
                                     catch (Exception ex)
@@ -225,7 +225,7 @@ namespace Download.Controllers
                                         //Get the readme
                                         var file = GetBlob("file", arch.ReadMe);
                                         //Convert the byte[] to text so the markdown converter will work
-                                        var Read = System.Text.Encoding.UTF8.GetString(file.ToArray());
+                                        var Read = System.Text.Encoding.GetEncoding(1252).GetString(file);
                                         ViewData["Content"] = md.Transform(Read);
                                     }
                                     catch (Exception ex)
